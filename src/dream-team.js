966cc9c1b7b1379bb1e2304fd33d5768e42a7505
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-04-15 10:53:23 +0300

    feat: add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam( members) {
-    if(!Array.isArray(members)) return false;
- let array=[]
- members.forEach(el=>{if(typeof(el)==='string') array.push(el[0])})
- return array.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join('').toUpperCase()
+// function createDreamTeam( members) {
+//     if(!Array.isArray(members)) return false;
+//  let array=[]
+//  members.forEach(el=>{if(typeof(el)==='string') array.push(el[0])})
+//  return array.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join('').toUpperCase()
+// }
+
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members
+    .map(el => {
+      if (typeof el === "string") return el.trim().charAt(0).toUpperCase();
+    })
+    .filter(el => el !== undefined).sort().join("");
 }
 
 module.exports = {
