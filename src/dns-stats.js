cc432564bcafe92204c65543a7a17725ff9f7852
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-04-15 23:14:39 +0300

    feat: add code

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+ function getDNSStats(domains) {
+  const dnsCounts = domains.reduce((acc, domain) => {
+    const parts = domain.split('.').reverse();
+    let dns = '';
+    for (const part of parts) {
+      dns = `${dns}.${part}`;
+      acc[dns] = (acc[dns] || 0) + 1;
+    }
+    return acc;
+  }, {});
+  return dnsCounts;
+}
 }
 
 module.exports = {
